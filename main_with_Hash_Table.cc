c9f17c79090ec98a789e662a379dd7dc031f99b7
Nicholas Williams <williamsnick606@users.noreply.github.com>
2017-05-08 04:00:51 -0700

    I rewrote main but it's not complete by far
    All it does at this point is accept and create key/value pairs in the hash table. When input is like : LET X = 11 : format i mean. 
    
    The inspiration to parse it this way came from the Bitfield project, this project seems similar in many ways to Bitfield and RPN Calculator imo. 
    
    My next goal is to create a method to accept the variables, numbers, and operators and put them into the int(number) deque and char(operators) deque. Once that is done it should be straight-forward to implement with the separated equation functionality, but don't quote me on that :)

--- main_with_Hash_Table.cc
+++ main_with_Hash_Table.cc
@@ -1,247 +1,126 @@
+//Main file for the hash table version.
+//This code will function independentely from the main_with_Hash_Table_Operations.
 #include <iostream>
-#include <ncurses.h>
+#include <string>
+#include <sstream>
 #include <vector>
-#include <ctype.h>
-#include <algorithm>
-#include <cmath>
 #include <unordered_map>
+#include <cctype>
+
+void die(); //Function Prototype
 
 using namespace std;
 
-class special_Hash_Table  // Pods class to hold hash table and do error checking on inputs
+//Custom hash table to increment all key value by one each time they are accessed.
+class special_Hash_Table
 {
-    public:
-    unordered_map<char,int> variable_Map;
-    void valid_Number(int num)
-    {
-        if(num>255)
-            // Write code to throw exception here instead of die()
-        cout<<"Bad input\n"; // Change this to throw an exception
-    }
-};
-void uppercaseify(string& mystr)
-{                              //Converts the passed in string to all uppercase.
-	for (auto& c: mystr)
-    {
-		c = toupper(c);
-	}
-}
-bool is_alpha(string& mystr)
-{                                  //True if the passed in string is all letters.
-    for (auto& c: mystr)
-        if(!(isalpha(c)))
-            return false;
-    return true;
-}
-bool is_digit(string& mystr)
-{
-    for (auto& c: mystr)
-        if(!(isdigit(c)))
-            return false;
-    return true;
-}
-unsigned int mod(int num,int modulus)
-{
-	while(num<0)
-	{
-		num+=modulus;
-	}
-	return num;
-}
-void die() 
-{                                                    //Exits program.
-    cout << "BAD INPUT" << endl;
-    exit(0);
-}
-int main() 
-{
-    special_Hash_Table Map;
-	int number;
-    float answer;
-    float scalor;
-    string variable_string;
-    string operation;
-    string a;
-    char operat;
-    string equals;
-    while(cin) 
-	{                                                //MAIN LOOP.
-        cin >> operation;
-        uppercaseify(operation);
-        if(operation == "QUIT") break;
-        if(operation == "LET")                              //Takes in "LET" to make sure they are creating variables.
- 		{
-            cin >> variable_string;                         // Get the variable and do error checking
-            if(variable_string.size()>1)
-                die();
-            char variable=toupper(variable_string[0]);
-            if(!isalpha(variable))
-                die();
-            if(Map.variable_Map.find(variable)!=Map.variable_Map.end()) // If it already exists then die
-                die();
-
-            cin>>equals;                                     // Get equal sign, and die if it's not equal sign
-            //if(equals!="=")
-              //  die();
-
-            cin >> number;                                   // Get number and do error checking on it
-            if(!cin)
-                die();
-
-            Map.variable_Map.insert(unordered_map<char,int>::value_type(variable,number)); // Insert a key/value pair into hash table if all things are good
+	public:
+		unordered_map<char,int> variable_Map;
+		
+		//This member function checks to see if the key already exists.
+		bool no_Duplicates(char variable)
+		{
+			auto it=variable_Map.find(variable); //Checking to make sure it exists every time.
+			if(it==variable_Map.end())
+				return true;
+			else
+				return false;;
 		}
 		
-		if(operation.size() == 1) 
+		//This member function can be passed a key and will return the corresponding value.
+		//It is also how the incrementation is implemented.
+		int return_Variable_Value(char variable)
 		{
-        	 while(cin) 
-			 {
-				 answer=Map.variable_Map.find(operation[0])->second;
-                cin >> operat;
-                switch(operat)
-				{
-					case '+':
-                    	cin >> a;
-                    	uppercaseify(a);
-						cout<<"\nwoooooow\n";
-                    	if(is_alpha(a)) 
-						{
-							auto it=Map.variable_Map.find(a[0]);
-							cout<<"\nhahahah\n";
-                        	if(it==Map.variable_Map.end())
-								die();
-							else
-							{
-								answer+=it->second;
-								it->second++;
-								cout<<endl<<answer<<endl;
-							}
-                   		}
-                    	else 
-						{
-						cout<<"\nTHIS PLACE\N";
-						//answer=Map.variable_Map.find(operation[0])->second;
-						scalor = atoi(a.c_str());               //Converts numbers in a string to an int.
-						answer += scalor;
-					}
-					break;
-				case '-':
-					cin >> a;
-                    	uppercaseify(a);
-                    	if(is_alpha(a))
-						{
-							auto it=Map.variable_Map.find(a[0]);
-                        	if(it==Map.variable_Map.end())
-                        		die();
-                        	else
-                        	{
-                        		answer-=it->second;
-                            	it->second++;
-                        	}
-						}
-                    	else 
-						{
-                        	scalor = atoi(a.c_str());
-                        	answer -= scalor;
-                   		}
-            			break;
-                	case '*':
-                    	cin >> a;
-                    	uppercaseify(a);
-                    	if(is_alpha(a))
-						{
-                            auto it=Map.variable_Map.find(a[0]);
-							if(it==Map.variable_Map.end())
-                            	die();
-                            else
-                            {
-                            	answer*=it->second;
-                            	it->second++;
-                            }
-                        }
-						else
-						{
-                        	scalor = atoi(a.c_str());
-                        	answer *= scalor;
-						}
-						break;
-          
-                	case'/':
-                    	cin >> a;
-                    	uppercaseify(a);
-                    	if(is_alpha(a))
-						{
-                         	auto it=Map.variable_Map.find(a[0]);
-							if(it==Map.variable_Map.end())
-                         		die();
-                         	else
-                         	{
-                         		answer/=it->second;
-                            	it->second++;
-                         	}
-                    	}
-                    	else 
-						{
-                    		scalor = atoi(a.c_str());
-                        	answer /= scalor;
-                    	}
-              			break;
-                	case '^':
-                    	cin >> a;
-                    	uppercaseify(a);
-                    	if(is_alpha(a))
-						{
-                            auto it=Map.variable_Map.find(a[0]);
-							if(it==Map.variable_Map.end())
-                            	die();
-                            else
-                            {
-                            	answer=pow(answer,it->second);
-                                it->second++;
-                            }
-                        }
-
-                    	else
-					    {
-                        	scalor = atoi(a.c_str());
-                        	answer = pow(answer, scalor);
-                    	}
-						break;
-                
-                	case '%':
-                    	cin >> a;
-                    	uppercaseify(a);
-                    	if(is_alpha(a))
-						{
-                            auto it=Map.variable_Map.find(a[0]);
-							if(it==Map.variable_Map.end())
-                            	die();
-                            else
-                            {
-                            	answer=mod(answer,it->second);
-                                it->second++;
-                            }
-                        }
-                    	else 
-						{
-                        	scalor = atoi(a.c_str());
-                        	answer=mod(static_cast<int>(answer),static_cast<int>(scalor));
-							//Casts the values as ints; can't use float values with (%).
-                    	}
-                	case '\n':
-                		break;
-            		default:
-						break;
-				}
-				cout << "answer = " << answer << endl;
-				break;
-        }
-        if(operation == "AS") // this is our way to see all the values in the hash table ( take this out later )
-		{
-			for(auto p:Map.variable_Map)
+			if(no_Duplicates(variable))
 			{
-				cout<<p.second<<" ";
+				auto it=variable_Map.find(variable); //Checking to make sure it exists every time.
+				int value= it->second;
+				it->second++;
+				return value;
 			}
+			else
+				die();
 		}
-	}// end of inner while loop
-} // end of while loop
-} // end of main
+		//This function cuts down the coding time to type insert.
+		//It also performs error checking on the number that is being inserted and will die if it is out of bounds. !!! CHANGE TO THROW EXCEPTION LATER !!!
+		void insert(char variable,int number)
+		{
+			if(number>255)
+				die(); // Throw exception here instead.
+			else
+				variable_Map.insert(unordered_map<char,int>::value_type(variable,number));
+		}
+};
+//I probably shouldn't make this global
+special_Hash_Table Map;
+
+void die()
+{
+	cout<<"\nBad Input\n";
+	exit(0);
+}
+void populate_Word_Vector(vector<string> &word_Vector)
+{
+    string input_line;
+    string word;
+    getline(cin,input_line);
+    stringstream ssInput_Line(input_line);
+    while(ssInput_Line>>word)
+    {
+        word_Vector.push_back(word);
+    }
+}
+void print(vector<string> &vec)
+{
+    for(string i:vec){cout<<i<<endl;}
+}
+bool is_A_Number(string &number_String)
+{
+	for(char c:number_String)
+	{
+		if(!isdigit(c))
+			return false;
+		else
+			continue;
+	}
+	return true;
+}
+void LET_Sequence(vector<string> &vec, int i=1)
+{
+	if(!(isalpha(vec.at(i)[0])&&vec.at(i).size()==1)) //If the next string isn't a variable then die.
+		die();
+	char variable=vec.at(i)[0];
+	if(!Map.no_Duplicates(variable))
+		die();
+	if(!(vec.at(i+1)=="="))
+		die();	
+	if(!(is_A_Number(vec.at(i+2))))
+		die();
+	int number=stoi(vec.at(i+2));
+	Map.insert(variable,number);
+	cout<<"\nThe value is : "<<Map.variable_Map.find(variable)->second<<endl; 
+	//^Just a little test to see that it is succseful in storing the key/value pair in the hash table :).
+}
+void do_Things(vector<string> &vec)
+{
+	//If it's "LET" or if it's a single letter or if it's a number as the first string, then it's safe to proceed, otherwise die.
+	if(!(vec.at(0)=="LET"||(isalpha(vec.at(0)[0])&&vec.at(0).size()==1)||is_A_Number(vec.at(0))))
+		die();
+	if(vec.at(0)=="LET") //Let sequence
+		LET_Sequence(vec,1);
+	//Need more here to complete this junk!! :(
+}
+
+int main()
+{
+    vector<string> words;
+    while(true)
+	{
+		populate_Word_Vector(words);
+		do_Things(words);
+	}
+
+    return 0;
+}
+
+
